16586fee678eaf442bc50c2dc511119b78db14d8
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-01-04 18:09:40 +0100

    Update maj.php

--- html/maj.php
+++ html/maj.php
@@ -39,8 +39,8 @@
     <main>
         <h2>04/01/2021 - V3.0.1</h2>
         <ul>
-            <li>Création d'un <a href="https://notes.iutmulhouse.uha.fr/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uIjoiQ29tcHRlX0RlbW8udGVzdEB1aGEuZnIiLCJzdGF0dXQiOiJldHVkaWFudCJ9.kHuiNx8X2mWUjv1LAHVOdcLGCu2yQS_i6fxqZZICuEA" >compte démo</a></li>
-            <li>Mise en ligne du code source sur <a href="https://github.com/SebL68">GitHub</a></li>
+            <li>Création d'un <a target=_blank href="https://notes.iutmulhouse.uha.fr/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uIjoiQ29tcHRlX0RlbW8udGVzdEB1aGEuZnIiLCJzdGF0dXQiOiJldHVkaWFudCJ9.kHuiNx8X2mWUjv1LAHVOdcLGCu2yQS_i6fxqZZICuEA" >compte démo</a></li>
+            <li>Mise en ligne du code source sur <a target=_blank href="https://github.com/SebL68/Scodoc_Notes">GitHub</a></li>
         </ul>
         <h2>01/01/2020 - V3.0.0</h2>
         <p>
