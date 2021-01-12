5a9ec21ff81245154a75bcec65f31490ebcc08ce
dg-68 <77330653+dg-68@users.noreply.github.com>
2021-01-12 14:40:20 +0100

    Update createJWT.php

--- html/services/createJWT.php
+++ html/services/createJWT.php
@@ -10,7 +10,7 @@
 	$authData = (object) authData();
 
 	if(
-		$authData->session != 'sebastien.lehmann@uha.fr' ||
+		$authData->session != 'sebastien.lehmann@uha.fr' &&
 		$authData->session != 'denis.graef@uha.fr'
 	){ 
 		die("Ce service n'est autorisé que pour Sébastien Lehmann, vous pouvez le contacter.");
@@ -22,8 +22,8 @@
 	include $path . '/includes/JWT/key.php';
 
 	$payload = [
-		'session' => 'Compte_Demo.test@uha.fr', // mail de la personne destinataire du jeton
-		'statut' => 'etudiant', // etudiant ou personnel
+		'session' => 'denis.graef@uha.fr', // mail de la personne destinataire du jeton
+		'statut' => 'personnel', // etudiant ou personnel
 		//'exp' => 1608498444 // (optionnel) timestamp d'expiration du tocken 
 	];
 	echo JWT::encode($payload, $key);
