c48e940549fa1431938a052d1d0e9e81cf1b3176
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-01-15 10:41:44 +0100

    Update data.php

--- html/services/data.php
+++ html/services/data.php
@@ -215,7 +215,8 @@
 			for($i=0 ; $i<count($json->insemestre) ; $i++){
 				$output[] = $json->insemestre[$i]->formsemestre_id;
 			}
-			return rsort($output);
+			rsort($output);
+			return $output;
 		}else{
 			returnError(
 				"Problème de compte, vous n'êtes pas dans Scodoc ou votre numéro d'étudiant est erroné, si le problème persiste, contactez votre responsable en lui précisant : il y a peut être un .0 à la fin du numéro d'étudiant dans Scodoc."
