5ceacba416cd4e99fb201dca3f5d94979eee247b
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-01-26 19:29:14 +0100

    Jetons étudiants
    Système pour que les étudiants puissent récupérer leur propre jeton JWT.
    Valide 6 mois.

---
+++ html/services/createStudentJWT.php
@@ -0,0 +1,24 @@
+<?php
+/***************************************/
+/* Service de création de tocken JWT 
+	pour des TP étudiants			   */
+/* https://github.com/firebase/php-jwt */
+/***************************************/
+
+	$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/..');
+	include "$path/includes/auth.php";
+
+	$authData = (object) authData();
+
+	use \Firebase\JWT\JWT;
+
+	include $path . '/includes/JWT/JWT.php';
+	include $path . '/includes/JWT/key.php';
+
+	$payload = [
+		'session' => $authData->session, // mail de la personne destinataire du jeton
+		'statut' => 'etudiant', 
+		'exp' => time() + (26*7*24*60*60) // Valide pour ce semestre (6 mois de plus par rapport à maintenant)
+	];
+	echo 'Votre jeton d\'accès est : <br>' . JWT::encode($payload, $key);
+?>
