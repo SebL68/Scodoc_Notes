bd055cc64dbeaf0c7c4b3aabbf70495107192198
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-07-10 02:09:03 +0200

    Adaptation responsive de la navigation

--- html/assets/header.php
+++ html/assets/header.php
@@ -1,9 +1,13 @@
 <header>
 	<h1><?php echo $h1; ?></h1>
-	<!--<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>-->
-	<a class="nav" id="notes" href="/">Notes</a>
-	<a class="nav" id="documents" href="/services/studentsLists.php">Documents</a>
-	<a class="nav" id="absences" href="/absences/">Absences</a>
-	<a class="nav" id="admin" href="/absences/admin.php">Stats / Admin</a>
-	<a href=logout.php>Déconnexion</a>
+
+	<nav>
+		<svg onclick="this.parentElement.parentElement.classList.toggle('ouvert')" xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+
+		<a class="nav" id="notes" href="/">Notes</a>
+		<a class="nav" id="documents" href="/services/studentsLists.php">Documents</a>
+		<a class="nav" id="absences" href="/absences/">Absences</a>
+		<a class="nav" id="admin" href="/absences/admin.php">Stats / Admin</a>
+		<a href=logout.php>Déconnexion</a>
+	</nav>
 </header>
