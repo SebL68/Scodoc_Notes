6de4c636f8c5c0b01198225a69f12e01f9af316c
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-10-28 15:47:02 +0200

    Update authentification.class.php

--- includes/authentification/authentification.class.php
+++ includes/authentification/authentification.class.php
@@ -2,6 +2,21 @@
 
 // https://www.pierre-giraud.com/php-mysql-apprendre-coder-cours/introduction-programmation-orientee-objet/
 
+/****************************/
+/* Class Auth
+	Classe permettant d'instancier le processus d'authentification
+
+	Si la personne n'est pas authentifiée, un JSON de redirection est renvoyé :
+		{
+			'redirect' => '/services/doAuth.php' // URL vers la page pour s'authentifier
+		}
+
+	Méthodes publiques :
+		Auth->getSessionName() : renvoie le mail de la personne => string 'jean.dupond@uha.fr'
+		Auth->getStatut() : renvoie le statut de la personne => int ETUDIANT | PERSONNEL | ADMINISTRATEUR | INCONNU
+		
+/****************************/
+
 	interface Authentification{
 		public function getSessionName();
 		public function getStatut();
