1da4afd18a9ea36eb2ccf34108afc4510c4e8a82
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-11-01 19:21:59 +0100

    Ajout des fichiers de config vierge

---
+++ config/analytics.php
@@ -0,0 +1,9 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX"></script>
+<script>
+	if(location.hostname != "localhost" && location.hostname != "127.0.0.1"){
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-XXXXXXXX', { 'anonymize_ip': true });
+	}
+</script>
