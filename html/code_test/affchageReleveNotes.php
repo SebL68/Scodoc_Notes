2797fcaebedb52d8c1b0783abf4ddc7bef35cf81
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-11-26 15:33:15 +0100

    Create affchageReleveNotes.php

---
+++ html/code_test/affchageReleveNotes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Relevé de notes</title>
+	<style>
+
+	</style>
+</head>
+<body spellcheck=true contenteditable=true>
+	<main>
+		<h2>Semestre</h2>
+		<em>Ces informations ne servent qu'à situer l'étudiant dans la promotion et ne reflètent pas à l'obtention du semestre.</em>
+		
+
+
+	</main>
+	<script>
+		let data = <?php include('releveNotes.json'); ?>;
+
+	</script>
+</body>
+</html>
