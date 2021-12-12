e1c4eb9c5bb0c38b05ad6d932d8e40d21716cabf
SebL68 <31206445+SebL68@users.noreply.github.com>
2021-12-12 21:59:14 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -20,8 +20,59 @@
 	/********************************/
 	/* Clé pour les jetons JWT      */
 	/********************************/
-		public static $JWT_key = 'Votre_clé_PWT_personnelle';
-		
+		public static $JWT_key = ''; // Clé pour les jetons ou rien pour désactiver
+
+	/********************************************/
+	/* Class à utiliser pour l'authentification */
+	/* On peut alors utiliser un autre système  */
+	/********************************************/
+		public static $auth_class = 'auth_CAS.class.php';	
+	
+	/*******************************************************/
+	/* Class à utiliser pour accéder au service d'annuaire */
+	/* On peut aussi utiliser un autre système             */
+	/*******************************************************/
+		public static $service_annuaire_class = 'service_annuaire_LDAP.class.php';
+
+	/**********************/
+	/* Configuration LDAP */
+	/**********************/
+	// Identifiants pour accéder au serveur LDAP
+		public static $LDAP_url = 'ldap://ldap.uha.fr:389';
+		public static $LDAP_user = 'uid=didev,ou=dsa,dc=uha,dc=fr';
+		public static $LDAP_password = 'MDP_LDAP';
+
+	// Désignation du Distinguished Name dans LDAP
+		public static $LDAP_dn = 'dc=uha,dc=fr';
+
+	// Champs LDAP utilisés pour les listes d'utilisateurs
+		public static $LDAP_uid = 'uid';      // Numéro d'étudiant ou d'enseignant
+		public static $LDAP_mail = 'mail';
+
+	// Filtre LDAP de l'UFR (supannaffectation)
+		public static $LDAP_filtre_ufr = 'supannaffectation=Institut Universitaire de Technologie de Mulhouse';
+	// Filtre LDAP étudiants (edupersonaffiliation)
+		public static $LDAP_filtre_statut_etudiant = 'edupersonaffiliation=student';
+	// Filtre LDAP enseignants (edupersonaffiliation)
+		public static $LDAP_filtre_enseignant = '&(!(edupersonaffiliation=staff))(edupersonaffiliation=teacher)(!(edupersonaffiliation=affiliate))(!(edupersonaffiliation=student))';
+	// Filtre LDAP BIATSS (edupersonaffiliation)
+		public static $LDAP_filtre_biatss = '&(edupersonaffiliation=staff)(!(edupersonaffiliation=teacher))(!(edupersonaffiliation=affiliate))';
+
+	/**********************************************************/
+	/* Class à utiliser pour gérer la planification de tâches */
+	/* On peut aussi utiliser un autre système                */
+	/**********************************************************/
+		public static $scheduler_class = 'scheduler_crontab.class.php';
+
+	/**************************/
+	/* Paramétrage de crontab */
+	/**************************/
+		public static $tmp_dir = '/tmp';			// Dossier temporaire utilisé lors de la programmation de CRON
+		public static $CRON_delay = '0 0 * * *';	// Périodicité de mise à jour des listes d'utilisateurs :
+													// '*/2 * * * *' => Toutes les 2 minutes
+													// '0 * * * *'   => Toutes les heures à xxh00
+													// '0 0 * * *'   => Tous les jours à 00h00
+
 	/********************************/
 	/* Accès à Scodoc               */
 	/********************************/
@@ -29,47 +80,20 @@
 
 		public static $scodoc_url = 'https://iutmscodoc9.uha.fr/ScoDoc';
 		public static $scodoc_login = [
-			'__ac_name' => "identifiant_Scodoc",
-			'__ac_password' => 'mot_de_passe'
+			'__ac_name' => 'utilisateurScodoc',
+			'__ac_password' => 'MDP_Scodoc'
 		];
+		
+		public static $scodoc_login2 = 'utilisateurScodoc';
+		public static $scodoc_psw = 'MDP_Scodoc';
 
 	/****************************/
 	/* Configuration du serveur */
 	/****************************/
-		public static $webServerUser = "www-data";	/* Nécessaire ? */
-		public static $webServerGroup = "www-data";	/* Nécessaire ? */
-		public static $PHP_cmd = "/usr/bin/php";
-
-		public static $tmp_dir = "/tmp";			// Dossier temporaire utilisé lors de la programmation de CRON
-		public static $CRON_delay = "0 0 * * *";	// Périodicité de mise à jour des listes d'utilisateurs :
-													// "*/2 * * * *" => Toutes les 2 minutes
-													// "0 * * * *"   => Toutes les heures à xxh00
-													// "0 0 * * *"   => Tous les jours à 00h00
-
-	/**********************/
-	/* Configuration LDAP */
-	/**********************/
-	// Identifiants pour accéder au serveur LDAP
-		public static $LDAP_url = "ldap://ldap.uha.fr:389";
-		public static $LDAP_user = "identifiant_LDAP";
-		public static $LDAP_password = "mot_de_passe_LDAP";
-
-	// Désignation du Distinguished Name dans LDAP
-		public static $LDAP_dn = "dc=uha,dc=fr";
-
-	// Champs LDAP utilisés pour les listes d'utilisateurs
-		public static $LDAP_uid = "uid";      // Numéro d'étudiant ou d'enseignant
-		public static $LDAP_mail = "mail";
-
-	// Filtre LDAP de l'UFR (supannaffectation)
-		public static $LDAP_filtre_ufr = "supannaffectation=Institut Universitaire de Technologie de Mulhouse";
-	// Filtre LDAP étudiants (edupersonaffiliation)
-		public static $LDAP_filtre_statut_etudiant = "edupersonaffiliation=student";
-	// Filtre LDAP enseignants (edupersonaffiliation)
-		public static $LDAP_filtre_enseignant = "&(!(edupersonaffiliation=staff))(edupersonaffiliation=teacher)(!(edupersonaffiliation=affiliate))(!(edupersonaffiliation=student))";
-	// Filtre LDAP BIATSS (edupersonaffiliation)
-		public static $LDAP_filtre_biatss = "&(edupersonaffiliation=staff)(!(edupersonaffiliation=teacher))(!(edupersonaffiliation=affiliate))";
-		
+		public static $webServerUser = 'www-data';	/* Nécessaire ? */
+		public static $webServerGroup = 'www-data';	/* Nécessaire ? */
+		public static $PHP_cmd = '/usr/bin/php';
+	
 	/************************/
 	/* Gestion des absences */
 	/************************/
