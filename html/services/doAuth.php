2030c84bf8ed851e775b444ce48b37428420d061
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-01-03 19:47:29 +0100

    Update doAuth.php

--- html/services/doAuth.php
+++ html/services/doAuth.php
@@ -1,7 +1,7 @@
 <?php
 	$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/..');
 	include_once $path.'/config/config.php';
-	include_once $path.'//includes/'.Config::$auth_class;	// Class Auth
+	include_once $path.'/includes/'.Config::$auth_class;	// Class Auth
 
 	Auth::doAuth();
 ?>
