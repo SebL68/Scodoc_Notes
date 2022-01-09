c7cbd3d8f766817c157d114b7d3e98ccdd230ccd
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-01-09 19:34:27 +0100

    Ref. compétence

--- html/code_test/referentielCompetences/ref_competences.js
+++ html/code_test/referentielCompetences/ref_competences.js
@@ -5,8 +5,10 @@ class ref_competences extends HTMLElement {
 		
 		/* Tempalate de base */
 		this.shadow.innerHTML = `
+			<h1></h1>
 			<div class=parcours></div>
 			<div class=competences></div>
+			<div class=ACs></div>
 		`;
 
 		/* Style du module */
@@ -18,36 +20,75 @@ class ref_competences extends HTMLElement {
 	}
 	
 	set setData(data){
-		this.parcours(data);
+		this.data = data;
+		this.shadow.querySelector("h1").innerText = "BUT " + data.specialite_long;
+		this.parcours();
 	}
 
-	parcours(data){
+	parcours(){
 		let parcoursDIV = this.shadow.querySelector(".parcours");
-		Object.entries(data.parcours).forEach(([cle, parcours])=>{
+		Object.entries(this.data.parcours).forEach(([cle, parcours])=>{
 			let div = document.createElement("div");
 			div.innerText = parcours.libelle;
-			div.addEventListener("click", ()=>{this.competences(data, cle)})
+			div.addEventListener("click", (event)=>{this.competences(event, cle)})
 			parcoursDIV.appendChild(div);
 		})
 		
 	}
 
-	competences(data, cle){
-		let output = "";
+	competences(event, cle){
+		this.shadow.querySelector(".parcours>.focus")?.classList.remove("focus");
+		event.currentTarget.classList.add("focus");
 
-		Object.values(data.parcours[cle].annees).forEach(annee=>{
-			output += `<div class=annee>`;
+		/* Récupère une liste plate de toutes les compentences de toutes les années */
+		let bucketCompetences = [
+			Object.keys(this.data.parcours[
+				Object.keys(this.data.parcours)[0]
+			].annees[1].competences) 
+			, Object.keys(this.data.parcours[cle].annees[2].competences) 
+			, Object.keys(this.data.parcours[cle].annees[3].competences)
+		].flat();
 
-			Object.keys(annee.competences).forEach(competence=>{
-				output += `<div>${competence}</div>`;
-			});
-
-			output += `</div>`;
+		/* Compte le nombre d'occurence de chaque compétence */
+		let competences = {};
+		bucketCompetences.forEach(competence=>{
+			competences[competence] = ++competences[competence] || 1
 		})
 
-
-		this.shadow.querySelector(".competences").innerHTML = output;
+		/* Affichage */
+		let numComp = 1;
+		this.shadow.querySelector(".competences").innerHTML = "";
+		Object.entries(competences).forEach(([competence, nb])=>{
+			var divCompetence3ans = document.createElement("div");
+			divCompetence3ans.className = "competence";
+			let numTmp = numComp;
+			for(let i=0 ; i<nb ; i++){
+				let divCompetence = document.createElement("div");
+				divCompetence.innerText = `${competence} ${i+1}`;
+				divCompetence.className = "comp" + numTmp;
+				divCompetence.addEventListener("click", (event)=>{this.AC(event, competence, numTmp, i+1)})
+				divCompetence3ans.appendChild(divCompetence);
+			}
+			this.shadow.querySelector(".competences").appendChild(divCompetence3ans);
+			numComp++;
+		})
 	}
 
+	AC(event, competence, numComp, annee){
+		event.currentTarget.classList.toggle("focus");
+		if(this.shadow.querySelector(`[data-competence="${competence} ${annee}"]`)){
+			this.shadow.querySelector(`[data-competence="${competence} ${annee}"]`).remove();
+		} else {
+			let output = `
+				<ul class=AC data-competence="${competence} ${annee}">
+					<h2 class=comp${numComp}>${competence} ${annee}</h2>
+			`;
+			Object.entries(this.data.competences[competence].niveaux["BUT"+annee].app_critiques).forEach(([num, contenu])=>{
+				output += `<li><div class=comp${numComp}>${num}</div><div>${contenu.libelle}</div></li>`;
+			})
+			this.shadow.querySelector(".ACs").innerHTML += output + "</ul>";
+		}
+		
+	}
 }
 customElements.define('ref-competences', ref_competences);
