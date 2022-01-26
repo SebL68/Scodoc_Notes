adcd467377af47bbcc5bc84d1b11e095d1cee24b
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-01-26 09:06:13 +0100

    Update createJWT.php

--- html/services/createJWT.php
+++ html/services/createJWT.php
@@ -29,11 +29,4 @@
 		'exp' => $exp // (optionnel) timestamp d'expiration du token
 	];
 	echo $root_url."?token=".JWT::encode($payload, $Config->JWT_key);
-
-/**********************************/
-/* Compte demo étudiant :
-	Compte_Demo.test@uha.fr
-	
-	eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uIjoiQ29tcHRlX0RlbW8udGVzdEB1aGEuZnIiLCJzdGF0dXQiOiJldHVkaWFudCJ9.kHuiNx8X2mWUjv1LAHVOdcLGCu2yQS_i6fxqZZICuEA
-*/
 ?>
