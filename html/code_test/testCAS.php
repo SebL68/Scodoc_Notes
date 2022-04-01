e08642104d077984391bf8782c13a358d0ca0fd3
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-04-01 14:40:52 +0200

    Create testCAS.php

---
+++ html/code_test/testCAS.php
@@ -0,0 +1,14 @@
+Authentification en cours ...
+<?php
+	$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/..');
+	require_once $path.'/includes/default_config.php';
+	
+	require_once $path . '/lib/CAS/CAS.php';
+	require_once $path . '/config/cas_config.php';
+	
+	phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+
+	phpCAS::forceAuthentication();
+
+	echo "C'est bien authentifié";
+?>
