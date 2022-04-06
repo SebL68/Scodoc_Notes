515e1e83db9a720f56d7e2ec0c0215b994429391
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-04-06 12:45:03 +0200

    Update testCAS.php

--- html/code_test/testCAS.php
+++ html/code_test/testCAS.php
@@ -1,12 +1,15 @@
-Authentification en cours ...
+Authentification en cours ...<br>
 <?php
 	$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/..');
 	require_once $path.'/includes/default_config.php';
 	
 	require_once $path . '/lib/CAS/CAS.php';
 	require_once $path . '/config/cas_config.php';
+
+	phpCAS::setDebug();
+	phpCAS::setVerbose(true);
 	
-	phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context, 'https://notes.iutmulhouse.uha.fr/');
+	phpCAS::proxy(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
 
 	phpCAS::forceAuthentication();
 
