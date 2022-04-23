dcb815d16555bcd03c2e96ae84d52251cfa696e4
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-04-23 15:48:29 +0200

    Update installOrUpdate.sh

--- installOrUpdate.sh
+++ installOrUpdate.sh
@@ -42,13 +42,21 @@ warn '   Attention, ce script est toujours en phase de test et donc fourni SANS 
 if $(which dpkg) -L apt  2>/dev/null | grep -q $(which apt-get); then
 	echo ' -- Système Debian ou Ubuntu - compatible avec le script'
 else
-	error 'Désolé ce script est conçu pour une distribution Debian ou Ubuntu ! Vous pouvez vous inspirer du script et de la documentation pour installer sur un autre système.'
+	error 'Désolé ce script est conçu pour une distribution Debian ou Ubuntu ! Vous pouvez vous inspirer du script et de la documentation pour installer sur un autre système'
 fi
 
 if [ -d "$INSTALLDIR/config" ]; then
 	warn 'Une installation de Scodoc_Notes semble déjà présente, mise à jour des dossiers html, includes et lib'
+
 else
-	warn 'Nouvelle installation détectée, installation complète des fichiers et des packages'
+	warn 'Nouvelle installation détectée, installation des packages'
+	apt -yq update
+	apt -yq install openssl apache2 wget unzip links
+	apt -yq install php php-curl php-xml php-ldap
+	a2enmod ssl
+	phpenmod ldap
+	systemctl restart apache2
+
 	doinstall=1
 fi
 
@@ -65,32 +73,24 @@ mv  Scodoc_Notes-main/includes .
 mv  Scodoc_Notes-main/lib .
 
 if [ $doinstall ]; then
-	rm -rf data config
 	mv Scodoc_Notes-main/data .
 	mv Scodoc_Notes-main/config .
 
-	success 'Patience, recherche et installation des packages nécessaires'
-	
-	apt -yq update
-	apt -yq install openssl apache2 wget unzip links
-	apt -yq install php php-curl php-xml php-ldap
-	a2enmod ssl
-	phpenmod ldap
-	systemctl restart apache2
-
+	echo 'Changement des droits sur le répetoire data'
 	chgrp -R www-data config data
 	chmod -R o-rx config data
 	chmod g+w data
 	cat << FIN 
-- Modifiez les fichiers /config/config.php et /config/cas_config.php suivant vos paramètres...
-- Puis redémarrez le serveur web par : sudo systemctl restart apache2
-- Vous pouvez diagnostiquer le fonctionnement de la passerelle avec un navigateur https://votre_serveur/services/diagnostic.php ou en mode texte avec :  
+L'installation automatique est terminée, maintenant, à vous de jouer :
+- modifiez les fichiers /config/config.php et /config/cas_config.php suivant vos paramètres,
+- redémarrez le serveur web par : sudo systemctl restart apache2,
+- pour vous aider, vous pouvez diagnostiquer le fonctionnement de la passerelle avec un navigateur https://votre_serveur/services/diagnostic.php ou en mode texte avec :  
 FIN
      	success 'links https://localhost/services/diagnostic.php'
 	echo
 	warn 'ATTENTION Il est fortement conseillé de changer le certificat auto-signé pour un vrai! Contactez votre DSI'
 else
-	echo 'Terminé.'
+	echo 'Mise à jour terminée'
 fi
 
 #Nettoyage
