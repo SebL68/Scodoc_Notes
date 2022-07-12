027353eef4d7c7cc3407709e3e984b86193e7dca
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-07-12 15:37:58 +0200

    Update test.php

--- html/test.php
+++ html/test.php
@@ -4,3 +4,7 @@
 	ini_set('display_errors', '1');
 	$path = realpath($_SERVER['DOCUMENT_ROOT'] . '/..');
 	require_once "$path/includes/default_config.php";
+
+	require_once "$path/includes/annuaire.class.php";
+
+	echo Annuaire::statut('sebastien.lehmann@uha.fr');
