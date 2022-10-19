649f38b9da0bffa76d5ce522b84ee1a7214e4309
SebL68 <31206445+SebL68@users.noreply.github.com>
2022-10-19 22:42:37 +0200

    Update auth_CAS.class.php

--- includes/auth_CAS.class.php
+++ includes/auth_CAS.class.php
@@ -69,7 +69,7 @@
 			}
 			phpCAS::forceAuthentication(); 
 
-			if( substr_compare( 'https://'.$_SERVER['HTTP_HOST'], $_GET['href'], 0) === 0 ) {	// Vérifier qu'on redifige vers le même domaine
+			if( substr_compare( $_GET['href'], 'https://'.$_SERVER['HTTP_HOST'], 0) === 0 ) {	// Vérifier qu'on redifige vers le même domaine
 				header('Location: '. $_GET['href']);
 			} else {
 				exit(
