706f4394fe550da72c097ee1bf8ce9f20ca42c41
SebL68 <31206445+SebL68@users.noreply.github.com>
2023-10-10 08:53:55 +0200

    Update absences.scodoc.class.php

--- includes/absences.scodoc.class.php
+++ includes/absences.scodoc.class.php
@@ -33,7 +33,7 @@
 						];
 					} else if(isset($response->errors[0]->message)) {
 						return [
-							'problem' => $response->errors[0]->message . ' - bug Scodoc, ce sera corrigé demain, l\'absence a été enregistrée.'
+							'problem' => $response->errors[0]->message . ' - bug Scodoc, ce sera corrigé demain, l\'absence a été enregistrée - pour visualiser, vous pouvez actualiser la page.'
 						];
 					}
 				break;
